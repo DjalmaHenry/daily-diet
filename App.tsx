35bf9e0bc26c3dc24540447ee5ab22971d7db53c
Djalma Henrique <contato@djalmahenry.com>
2022-12-26 21:22:24 -0300

    feat: add patterns

--- App.tsx
+++ App.tsx
@@ -1,9 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './src/screens/Home';
+import { ThemeProvider } from "styled-components";
+import  { ActivityIndicator } from "react-native";
+import {
+  useFonts,
+  NunitoSans_400Regular,
+  NunitoSans_700Bold,
+} from "@expo-google-fonts/nunito-sans";
+import theme from "./src/theme";
+import Home from "@screens/Home";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold });
+
   return (
-    <Home />
+    <ThemeProvider theme={theme}>
+      {fontsLoaded ? <Home /> : <ActivityIndicator />}
+    </ThemeProvider>
   );
 }
