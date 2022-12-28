8fa87dc567a5a9450846de48ba9da06f8fd4610c
Djalma Henrique <contato@djalmahenry.com>
2022-12-28 15:06:22 -0300

    feat: add meal props and state

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { useState } from "react";
 import * as S from "./styles";
 import LogoImg from "@assets/logo.png";
 import Highlight from "@components/Highlight";
 import Button from "@components/Button";
+import { FlatList } from "react-native";
+
+type MealProps = {
+  name: string;
+  description: string;
+  date: string;
+  time: string;
+  insideDiet: boolean;
+};
 
 export default function Home() {
+  const [meals, setMeals] = useState<MealProps[]>([
+    {
+      name: "Café da manhã",
+      description: "Pão, leite, café",
+      date: "10/10/2020",
+      time: "08:00",
+      insideDiet: true,
+    },
+  ]);
   return (
     <S.Container>
       <S.HeaderContainer>
